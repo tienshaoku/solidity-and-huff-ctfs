2c7e2bdfdd21fd3f959df209b58161684ccbc1b6
tienshaoku <tienshaoku@gmail.com>
2025-01-01 19:02:13 +0800

    Impersonator and Carousel

---
+++ script/MagicAnimalCarousel.s.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "../test/MagicAnimalCarousel.t.sol";
+
+contract MagicAnimalCarouselScript is Script {
+    function run() external {
+        vm.startBroadcast();
+        new MiddleMan(vm.envAddress("MAGIC_ANIMAL_CAROUSEL"));
+        vm.stopBroadcast();
+    }
+}
