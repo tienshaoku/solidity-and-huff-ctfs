30727ead52a5bb21f0075db0f8ac3609da949777
tienshaoku <tienshaoku@gmail.com>
2026-01-01 22:11:38 +0000

    refactor: Recovery & MagicNumber

--- test/openzeppelin-ethernaut/MagicNumber.t.sol
+++ test/openzeppelin-ethernaut/MagicNumber.t.sol
@@ -4,32 +4,42 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "src/openzeppelin-ethernaut/MagicNumber.sol";
 
-// tutorial/solution: https://www.cyfrin.io/glossary/simple-bytecode-contract-solidity-code-example
+// tutorial/solution: https://www.cyfrin.io/glossary/simple-deploymentBytecode-contract-solidity-code-example
 // command: cast send --mnemonic-path <path> --rpc-url <rpc-url> --create 69602A60005260206000F3600052600A6016F3
 
 contract MagicNumberTest is Test {
+    MagicNum instance;
+
+    function setUp() public {
+        instance = new MagicNum();
+    }
+
     function test() public {
-        // run time code = 0x602A60005260206000F3
-        bytes memory deploymentBytecode = hex"69602A60005260206000F3600052600A6016F3";
-        address deployedAddress;
+        assertEq(instance.solver(), address(0));
+
+        // runtime code = 602A60005260206000F3
+        // bytecode runs once during deployment, executes constructor logic, then returns the runtime bytecode
+        bytes
+            memory deploymentBytecode = hex"69602A60005260206000F3600052600A6016F3";
+        address deployed;
 
         assembly {
-            // eth value, offset, size
-            deployedAddress := create(0, add(deploymentBytecode, 0x20), 0x13)
+            // eth value, offset (there's a 32-byte length prefix), size
+            deployed := create(
+                0,
+                add(deploymentBytecode, 0x20),
+                // load size by pointing at deploymentBytecode and will find the 32-byte length prefix
+                mload(deploymentBytecode)
+            )
         }
-        // console.log("deployedAddress", deployedAddress);
+        // console.log("deployed", deployed);
 
-        (bool success, bytes memory result) = deployedAddress.call("");
+        (bool success, bytes memory result) = deployed.call("");
         assertEq(success, true);
         assertEq(result, abi.encode(uint256(42)));
+        assertEq(deployed.code.length, 10);
 
-        uint256 contractSize;
-        assembly {
-            contractSize := extcodesize(deployedAddress)
-        }
-        assertEq(contractSize, 10);
-
-        console.logBytes(abi.encodeWithSignature("allowance(address,address)"));
-        console.logBytes(abi.encodeWithSignature("transferFrom(address,address,uint256)"));
+        instance.setSolver(deployed);
+        assertEq(instance.solver(), deployed);
     }
 }
--- test/openzeppelin-ethernaut/Recovery.t.sol
+++ test/openzeppelin-ethernaut/Recovery.t.sol
@@ -1,2 +1,35 @@
-// solution:
-// simply look up etherscan to find the address of the contract created with Recovery.generateToken()
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "src/openzeppelin-ethernaut/Recovery.sol";
+
+// the real solution:
+// simply look up etherscan to find the address created with Recovery.generateToken()
+// the below is a demonstration of finding out address with foundry
+contract RecoveryTest is Test {
+    Recovery instance;
+
+    function setUp() public {
+        instance = new Recovery();
+    }
+
+    function test(string memory _name, uint256 _initialSupply) public {
+        uint64 nonceBefore = vm.getNonce(address(instance));
+        instance.generateToken(_name, _initialSupply);
+        address created = vm.computeCreateAddress(
+            address(instance),
+            nonceBefore
+        );
+        assertTrue(created.code.length != 0);
+
+        uint256 initialValue = 0.001 ether;
+        created.call{value: initialValue}("");
+        assertEq(created.balance, initialValue);
+
+        created.call(
+            abi.encodeWithSelector(SimpleToken.destroy.selector, address(this))
+        );
+        assertEq(created.balance, 0);
+    }
+}
