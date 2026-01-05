deb6d4f750d3352007bdbf62355d08bf3be5f294
tienshaoku <tienshaoku@gmail.com>
2026-01-05 22:05:31 +0000

    refactor: DoubleEntryPoint

--- test/openzeppelin-ethernaut/DoubleEntryPoint.t.sol
+++ test/openzeppelin-ethernaut/DoubleEntryPoint.t.sol
@@ -5,45 +5,65 @@ import "forge-std/Test.sol";
 import "src/openzeppelin-ethernaut/DoubleEntryPoint.sol";
 
 contract DetectionBot is IDetectionBot {
-    Forta forta;
+    address forta;
     address player;
 
     constructor(address _forta, address _player) {
-        forta = Forta(_forta);
+        forta = _forta;
         player = _player;
     }
 
-    function handleTransaction(address player, bytes calldata msgData) external override {
-        forta.raiseAlert(player);
+    function handleTransaction(
+        address player,
+        bytes calldata msgData
+    ) external override {
+        Forta(forta).raiseAlert(player);
     }
 }
 
+// fortaNotify() uses the registered player regardless of caller of delegateTransfer
+// as long as DetectionBot registered by player implements handleTransaction() and raiseAlert(),
+// no one can sweepToken(any token)
 contract DoubleEntryPointTest is Test {
     DoubleEntryPoint instance;
     CryptoVault cryptoVault;
     Forta forta;
     LegacyToken legacyToken;
+    uint256 initialBalance = 100 ether;
 
     function setUp() public {
         legacyToken = new LegacyToken();
         forta = new Forta();
         cryptoVault = new CryptoVault(address(this));
-        instance = new DoubleEntryPoint(address(legacyToken), address(cryptoVault), address(forta), address(this));
+        instance = new DoubleEntryPoint(
+            address(legacyToken),
+            address(cryptoVault),
+            address(forta),
+            address(this)
+        );
 
         cryptoVault.setUnderlying(address(instance));
         legacyToken.delegateToNewContract(DelegateERC20(address(instance)));
-        legacyToken.mint(address(cryptoVault), 100 ether);
+        legacyToken.mint(address(cryptoVault), initialBalance);
     }
 
     function test() public {
-        DetectionBot detectionBot = new DetectionBot(address(forta), address(this));
-
-        instance.forta().setDetectionBot(address(detectionBot));
-        assertEq(address(instance.forta().usersDetectionBots(address(this))), address(detectionBot));
+        DetectionBot detectionBot = new DetectionBot(
+            address(forta),
+            address(this)
+        );
+        forta.setDetectionBot(address(detectionBot));
+        assertEq(
+            address(forta.usersDetectionBots(address(this))),
+            address(detectionBot)
+        );
 
         vm.expectRevert();
         cryptoVault.sweepToken(IERC20(legacyToken));
+        assertEq(legacyToken.balanceOf(address(cryptoVault)), initialBalance);
 
-        assertTrue(instance.balanceOf(instance.cryptoVault()) == 100 ether);
+        vm.prank(makeAddr("alice"));
+        vm.expectRevert();
+        cryptoVault.sweepToken(IERC20(legacyToken));
     }
 }
